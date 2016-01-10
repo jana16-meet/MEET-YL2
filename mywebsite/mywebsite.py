64eb5f85e15208e18f93155e61f1efcffe2ecd8a
Your Name <you@example.com>
2016-01-10 16:09:26 +0200

    ggfhftyh

--- mywebsite/mywebsite.py
+++ mywebsite/mywebsite.py
@@ -2,17 +2,25 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def homepage():
 	return render_template('homepage.html')
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-@app.route("/hello/", methods=['POST'])
+
+@app.route("/contactme/")
+def contactme():
+	return render_template('contactme.html')
+
+@app.route("/aboutme/")
+def aboutme():
+	return render_template('aboutme.html')
+
+
+
+@app.route("/datapage/", methods=['POST'])
 def hello(name=None):
 	if ('name' in request.form.keys()):
 		name = request.form['name']
-	return render_template('datapage.html', name=name)
-
+		return render_template('datapage.html', name=name)
 
 
 if __name__ == "__main__":
